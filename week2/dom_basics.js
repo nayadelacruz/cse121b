8b89b118bcc000cce169f796d342c0728edf36f9
nayadelacruz <del16001@byui.edu>
2023-09-21 12:41:08 -0700

    week2 activity created

---
+++ week2/dom_basics.js
@@ -0,0 +1,19 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Description");
+document.body.appendChild(newImage);
+const newSection = document.createElement("section");
+const newH2 = document.createElement('h2');
+newH2.innerText="CSE 121b";
+newSection.appendChild(newH2);
+const newP = document.createElement('p');
+newP.innerText='Welcome to Javascript Language';
+newSection.appendChild(newP);
+
+document.body.appendChild(newSection);
+//const newSection = document.createElement("section");
+//newSection.innerHTML = "<h2>CSE 121b</h2><p>Welcome to Javascript Language</p>";
+//document.body.appendChild(newSection);
